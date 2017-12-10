c3391fe7a03eac03fc7eadb6fc08d82b849ae038
yuben75 <yuben75@gmail.com>
2017-12-10 18:17:55 +0800

    added pkg-config

--- mk.sh
+++ mk.sh
@@ -3,6 +3,7 @@
 #gcc *.c -o pwm-demo -lglib-2.0 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include
 if [ $1 = "-d" ]; then
 	gcc hello_glib.c -o glib-test_debug -lglib-2.0 -I/usr/lib/i386-linux-gnu/glib-2.0/ -I/usr/lib/i386-linux-gnu/glib-2.0/include -I/usr/include/glib-2.0
+	gcc hello_glib.c `pkg-config --cflags --libs glib-2.0 gthread-2.0`  -o hello
 elif [ $1 = "-r" ]; then
 
     cd /sitara/ti-sdk-am335x-evm/linux-devkit/
@@ -11,7 +12,6 @@ elif [ $1 = "-r" ]; then
 	cd /root/Desktop/80618_project/glibtest
 	#arm-linux-gnueabihf-gcc hello_glib.c -o glib-test_release -lglib-2.0 -I/usr/lib/i386-linux-gnu/glib-2.0/ -I/usr/lib/i386-linux-gnu/glib-2.0/include -I/usr/include/glib-2.0
     arm-linux-gnueabihf-gcc hello_glib.c -o glib-test_release -lglib-2.0 -I/sitara/ti-sdk-am335x-evm/linux-devkit/sysroots/armv7ahf-vfp-neon-3.2-oe-linux-gnueabi/usr/lib/glib-2.0/include/ -I/sitara/ti-sdk-am335x-evm/linux-devkit/sysroots/armv7ahf-vfp-neon-3.2-oe-linux-gnueabi/usr/include/glib-2.0/
-
 fi
 exit 0
 
